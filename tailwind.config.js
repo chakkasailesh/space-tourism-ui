0cd8ca599ff89b3c1c12290f3a1fdfcbca84ebc1
chakkasailesh <chakkasailesh444@gmail.com>
2024-01-02 22:58:55 +0530

    initial project setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {},
+    colors: {
+      dark: '#0B0D17',
+      light: '#D0D6F9',
+    },
+    fontFamily: {
+      signature: ['Bellefair'],
+    },
+  },
+  plugins: [],
+}
