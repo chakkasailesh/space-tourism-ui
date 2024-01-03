002160d38cce562f8a2ee618a0c57749becfe4a1
chakkasailesh <chakkasailesh444@gmail.com>
2024-01-03 22:32:05 +0530

    added empty components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { NavBar, Home, Destination, Crew, Technology } from './components'
 
 const App = () => {
-  return <div className="text-3xl font-signature text-dark">Hello World!</div>
+  return (
+    <div>
+      <NavBar />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/destination" element={<Destination />} />
+          <Route path="/crew" element={<Crew />} />
+          <Route path="/technology" element={<Technology />} />
+        </Routes>
+      </div>
+    </div>
+  )
 }
 
 export default App
---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+import Home from './Home'
+import Destination from './Destination'
+import Crew from './Crew'
+import Technology from './Technology'
+import NavBar from './NavBar'
+
+export { Home, Destination, Crew, Technology, NavBar }
